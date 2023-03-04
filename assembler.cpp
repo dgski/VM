dd23fb6b165e0ffe9b3b1d88cb63a6569f5e7901
David Gorski <gorski.dave@gmail.com>
2023-03-04 13:18:44 -0500

    Assembler data and labels first pass implementation

--- assembler.cpp
+++ assembler.cpp
@@ -1,6 +1,82 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
+#include <vector>
+
+struct SplitLine {
+  std::string_view one;
+  std::string_view two;
+  std::string_view three;
+};
+SplitLine splitLine(std::string_view line, char delimeter)
+{
+  SplitLine result;
+
+  const auto getNextToken = [](std::string_view& output, std::string_view& input) {
+    if (input.empty()) {
+      return;
+    }
+
+    if (const auto firstSpaceIndex = input.find(' '); firstSpaceIndex == std::string::npos) {
+      output = input;
+      input = std::string_view();
+    } else {
+      output = std::string_view(input.data(), firstSpaceIndex);
+      input.remove_prefix(firstSpaceIndex + 1);
+    }
+  };
+
+  getNextToken(result.one, line);
+  getNextToken(result.two, line);
+  getNextToken(result.three, line);
+  return result;
+}
+
+bool isInstruction(std::string_view line)
+{
+  return (line == "READ") || (line == "LOAD") || (line == "STORE") || (line == "COPY") || (line == "ADD") || (line == "JG") || (line == "SYS");
+}
+
+bool isData(std::string_view line)
+{
+  return line.front() == '.';
+}
+
+struct LabelsAndData {
+  std::unordered_map<std::string, int> labelToAddress;
+  std::unordered_map<std::string, int> dataToAddress;
+};
+LabelsAndData getLabelsAndData(const char* inputAssemblyFilePath)
+{
+  LabelsAndData result;
+
+  int currentMemoryAddress = 0;
+  std::string line;
+  std::ifstream assemblyFileStream(inputAssemblyFilePath);
+  while (std::getline(assemblyFileStream, line)) {
+    if (line.empty()) {
+      continue;
+    }
+
+    if (auto [one, two, three] = splitLine(line, ' '); isInstruction(one)) {
+      ++currentMemoryAddress;
+      continue;
+    } else if (isData(one)) {
+      one.remove_prefix(1);
+      result.dataToAddress[std::string(one)] = currentMemoryAddress++;
+    } else /*isLabel*/ {
+      result.labelToAddress[std::string(one)] = ++currentMemoryAddress;
+    }
+  }
+
+  return result;
+}
+
+std::vector<char> generateMachineCode(const char* inputAssemblyFilePath, const LabelsAndData& labelsAndData)
+{
+  return {};
+}
 
 int main(int argc, const char** argv)
 {
@@ -9,12 +85,15 @@ int main(int argc, const char** argv)
     return EXIT_FAILURE;
   }
 
-  const char* inputAssemblyFilePath = argv[2];
-  const char* bytecodeOutputFilePath = argv[3];
+  const char* inputAssemblyFilePath = argv[1];
+  const char* bytecodeOutputFilePath = argv[2];
 
-  std::string line;
-  std::ifstream assemblyFileStream(inputAssemblyFilePath);
-  while (std::getline(assemblyFileStream, line)) {
+  const auto labelsAndData = getLabelsAndData(inputAssemblyFilePath);
+  const auto machineCode = generateMachineCode(inputAssemblyFilePath, labelsAndData);
+
+  std::ofstream outputFile(bytecodeOutputFilePath);
+  for (auto byte : machineCode) {
+    outputFile.put(byte);
   }
 
   return EXIT_SUCCESS;
